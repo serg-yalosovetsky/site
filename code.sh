0d505777576e75533d4a486ba435517367c36e0c
Ubuntu <ubuntu@ip-172-31-27-139.us-east-2.compute.internal>
2022-02-11 12:30:54 +0000

    exec

--- code.sh
+++ code.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update
 sudo apt install aptitude -y
-sudo aptitude install zsh magic-wormhole nettools httpie lnav micro -y
+sudo aptitude install zsh magic-wormhole net-tools python3 pip httpie lnav micro -y
 sudo aptitude upgrade
