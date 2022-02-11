a0d6daab73cf8055ea3ddb8e00c08d627b08a912
serg-yalosovetsky <serg.yalosovetsky@gmail.com>
2022-02-11 14:48:50 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -3,3 +3,4 @@ sudo apt update
 sudo apt install aptitude -y
 sudo aptitude install zsh magic-wormhole net-tools python3 pip httpie lnav micro -y
 sudo aptitude upgrade
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
